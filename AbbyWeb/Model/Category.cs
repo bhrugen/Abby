036683e05522e928c7c1439940dc430bbb18d69d
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-08 14:55:30 -0500

    Section 2 - Data Annotations

--- AbbyWeb/Model/Category.cs
+++ AbbyWeb/Model/Category.cs
@@ -1,8 +1,12 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace AbbyWeb.Model;
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public int DisplayOrder { get; set; }
 }
