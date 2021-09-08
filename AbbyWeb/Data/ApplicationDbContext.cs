80ebf41741b7cf612e45882fe8d735424228bae8
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-08 15:06:51 -0500

    Section 2 - ApplicationDbContext

---
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿
+using AbbyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbyWeb.Data;
+public class ApplicationDbContext : DbContext
+{
+
+    public DbSet<Category> Category { get; set; }
+}
