a105b9c03e7dc0869c9c1a5c0195c60a631f0816
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-08 15:13:53 -0500

    Section 2 - Setup Program.cs to use DbContext

--- AbbyWeb/Data/ApplicationDbContext.cs
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 namespace AbbyWeb.Data;
 public class ApplicationDbContext : DbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
 
+    }
     public DbSet<Category> Category { get; set; }
 }
--- AbbyWeb/Program.cs
+++ AbbyWeb/Program.cs
@@ -1,8 +1,14 @@
 
+using AbbyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
