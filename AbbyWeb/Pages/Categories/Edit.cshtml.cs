5ffae3e5c24e44ab50a6259f1c6d201cd3c87c76
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-09 14:21:58 -0500

    Section 3 - Edit Category

---
+++ AbbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories;
+
+[BindProperties]
+public class EditModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    
+    public Category Category { get; set; }
+
+    public EditModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public void OnGet(int id)
+    {
+        Category = _db.Category.Find(id);
+        //Category = _db.Category.FirstOrDefault(u=>u.Id==id);
+        //Category = _db.Category.SingleOrDefault(u=>u.Id==id);
+        //Category = _db.Category.Where(u => u.Id == id).FirstOrDefault();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (Category.Name == Category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Category.Name", "The DisplayOrder cannot exactly match the Name.");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Category.Update(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+        return Page();
+    }
+}
