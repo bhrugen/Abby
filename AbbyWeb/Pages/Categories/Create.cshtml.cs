d4666627ac80b18665614102b64b5b674231425a
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-09 11:40:34 -0500

    Section 3 - Create Property in Page Model

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AbbyWeb.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,7 @@ namespace AbbyWeb.Pages.Categories;
 
 public class CreateModel : PageModel
 {
+    public Category Category { get; set; }
     public void OnGet()
     {
     }
