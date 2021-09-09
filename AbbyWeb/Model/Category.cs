7d2c1bd869a6034943e9e66add916f5ce795f56c
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-09 12:45:56 -0500

    Section 3 - Display Name and Range Annotation

--- AbbyWeb/Model/Category.cs
+++ AbbyWeb/Model/Category.cs
@@ -8,5 +8,7 @@ public class Category
     public int Id { get; set; }
     [Required]
     public string Name { get; set; }
+    [Display(Name="Display Order")]
+    [Range(1,100,ErrorMessage ="Display order must be in range of 1-100!!!")]
     public int DisplayOrder { get; set; }
 }
