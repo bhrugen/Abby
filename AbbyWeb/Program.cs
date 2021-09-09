e8db86fda03bd757080bdbe22582cca96130e384
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-09 11:18:23 -0500

    Section 3 - Display all categories

--- AbbyWeb/Program.cs
+++ AbbyWeb/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
