03d55d0b1180086bce74beb9ab4f0dfab733ce14
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-19 17:53:48 -0500

    Section 5 - Implement Category Repository

---
+++ Abby.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using Abby.DataAccess.Data;
+using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abby.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db= db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Category.FirstOrDefault(u => u.Id == category.Id);
+            objFromDb.Name = category.Name;
+            objFromDb.DisplayOrder = category.DisplayOrder;
+        }
+    }
+}
---
+++ Abby.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Abby.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abby.DataAccess.Repository.IRepository
+{
+    internal interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+        void Save();
+    }
+}
