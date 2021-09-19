cf60e0daf17624c48bd9b58d3e6126c0cd4ef0a3
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-19 17:25:27 -0500

    Section 5 - Create IRepository

---
+++ Abby.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abby.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //GET ALL, GET By ID FIRST OR DEFAULT, ADD, REMOVE, REMOVERANGE
+
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
+    }
+}
