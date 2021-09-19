80e5de6374d27953c137a16867d6d705cf268a7e
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-19 18:13:51 -0500

    Section 5  - Unit of Work Implementation

---
+++ Abby.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abby.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork :IDisposable
+    {
+        ICategoryRepository Category {  get; }
+        void Save();
+    }
+}
---
+++ Abby.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Abby.DataAccess.Data;
+using Abby.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abby.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+
+        public ICategoryRepository Category {get;private set;}
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
