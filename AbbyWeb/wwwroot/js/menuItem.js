8b46344439d0f52185abb81a64fac5f51e4c28db
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-20 10:18:08 -0500

    Section 6 - Load Datatables

--- AbbyWeb/wwwroot/js/menuItem.js
+++ AbbyWeb/wwwroot/js/menuItem.js
@@ -1,4 +1,17 @@
 ﻿
 $(document).ready(function () {
-    $('#DT_load').DataTable();
+    $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/MenuItem",
+            "type": "GET",
+            "datatype":"json"
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "price", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            { "data": "foodType.name", "width": "15%" }
+        ],
+        "width":"100%"
+    });
 });
