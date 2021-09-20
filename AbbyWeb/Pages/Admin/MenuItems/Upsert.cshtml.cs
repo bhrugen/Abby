656e3efe13e285fcf0e8713c596f5048078aee7c
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-19 21:23:03 -0500

    Section 6 - Menu Item Create Post Handler

--- AbbyWeb/Pages/Admin/MenuItems/Upsert.cshtml.cs
+++ AbbyWeb/Pages/Admin/MenuItems/Upsert.cshtml.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Abby.DataAccess.Data;
 using Abby.DataAccess.Repository.IRepository;
 using Abby.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -15,14 +17,16 @@ namespace AbbyWeb.Pages.Admin.MenuItems;
 public class UpsertModel : PageModel
 {
     private readonly IUnitOfWork _unitOfWork;
-    
+    private readonly IWebHostEnvironment _hostEnvironment;
+
     public MenuItem MenuItem { get; set; }
     public IEnumerable<SelectListItem> CategoryList { get;set;  }
     public IEnumerable<SelectListItem> FoodTypeList { get; set; }
 
-    public UpsertModel(IUnitOfWork unitOfWork)
+    public UpsertModel(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
     {
         _unitOfWork = unitOfWork;
+        _hostEnvironment = hostEnvironment;
         MenuItem = new();
     }
     public void OnGet()
@@ -41,14 +45,29 @@ public class UpsertModel : PageModel
 
     public async Task<IActionResult> OnPost()
     {
-      
-        //if (ModelState.IsValid)
-        //{
-        //    _unitOfWork.FoodType.Add(FoodType);
-        //    _unitOfWork.Save();
-        //    TempData["success"] = "FoodType created successfully";
-        //    return RedirectToPage("Index");
-        //}
-        return Page();
+
+        string webRootPath = _hostEnvironment.WebRootPath;
+        var files = HttpContext.Request.Form.Files;
+        if (MenuItem.Id == 0)
+        {
+            //create
+            string fileName_new = Guid.NewGuid().ToString();
+            var uploads = Path.Combine(webRootPath, @"images\menuItems");
+            var extension = Path.GetExtension(files[0].FileName);
+
+            using (var fileStream = new FileStream(Path.Combine(uploads, fileName_new + extension), FileMode.Create))
+            {
+                files[0].CopyTo(fileStream);
+            }
+            MenuItem.Image = @"\images\menuItems\" + fileName_new + extension;
+            _unitOfWork.MenuItem.Add(MenuItem);
+            _unitOfWork.Save();
+        }
+        else
+        {
+            //edit
+        }
+
+        return RedirectToPage("./Index");
     }
 }
