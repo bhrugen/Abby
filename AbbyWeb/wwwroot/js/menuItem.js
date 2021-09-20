117784c6d106d72bba5970d2f7b36b9f96e9517f
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-20 10:29:25 -0500

    Section 6 - DataTable Render Buttons

--- AbbyWeb/wwwroot/js/menuItem.js
+++ AbbyWeb/wwwroot/js/menuItem.js
@@ -10,7 +10,20 @@ $(document).ready(function () {
             { "data": "name", "width": "25%" },
             { "data": "price", "width": "15%" },
             { "data": "category.name", "width": "15%" },
-            { "data": "foodType.name", "width": "15%" }
+            { "data": "foodType.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" >
+                            <a href="/Admin/MenuItems/upsert?id=${data}"  class="btn btn-success text-white mx-2">
+                            <i class="bi bi-pencil-square"></i>  </a>
+                            <a href="/Admin/MenuItems/upsert?id=${data}"  class="btn btn-danger text-white mx-2">
+                             <i class="bi bi-trash-fill"></i>  </a>
+                            </div>`
+                },
+
+                "width": "15%"
+            }
         ],
         "width":"100%"
     });
