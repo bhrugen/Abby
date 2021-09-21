7df0e7e86deaaf3c4de88f65d5649315d8a4831c
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-21 15:44:46 -0500

    Section 11 - Add Stripe in Page Model

--- AbbyWeb/Pages/Customer/Cart/Summary.cshtml.cs
+++ AbbyWeb/Pages/Customer/Cart/Summary.cshtml.cs
@@ -4,8 +4,10 @@ using Abby.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe.Checkout;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 
 namespace AbbyWeb.Pages.Customer.Cart
@@ -41,7 +43,7 @@ namespace AbbyWeb.Pages.Customer.Cart
             }
         }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
@@ -76,10 +78,45 @@ namespace AbbyWeb.Pages.Customer.Cart
                     _unitOfWork.OrderDetail.Add(orderDetails);
                     
 				}
-
+                int quantity = ShoppingCartList.ToList().Count;
                 _unitOfWork.ShoppingCart.RemoveRange(ShoppingCartList);
                 _unitOfWork.Save();
+
+
+                 var domain = "http://localhost:4242";
+                var options = new SessionCreateOptions
+                {
+                    LineItems = new List<SessionLineItemOptions>
+                {
+                  new SessionLineItemOptions
+                  {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount= (long)OrderHeader.OrderTotal*100,
+                        Currency="usd",
+                        ProductData= new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = "Abby Food Order"
+                        },
+                    },
+                    Quantity = quantity
+                  },
+                },
+                    PaymentMethodTypes = new List<string>
+                {
+                  "card",
+                },
+                    Mode = "payment",
+                    SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={OrderHeader.Id}",
+                    CancelUrl = domain + "customer/cart/index",
+                };
+                var service = new SessionService();
+				Session session = service.Create(options);
+                Response.Headers.Add("Location", session.Url);
+                return new StatusCodeResult(303);
             }
+
+            return Page();
         }
     }
 }
