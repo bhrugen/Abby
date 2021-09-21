92c6b80a01099ca144a653d0e1db2174da6a7c8a
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-21 11:36:25 -0500

    Section 9 - Cart Summary UI

---
+++ AbbyWeb/Pages/Customer/Cart/Summary.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Customer.Cart
+{
+    public class SummaryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
