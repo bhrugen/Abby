5e7e3f9db2a4c4cae33c329eec4efa6d5a3aaa49
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-22 11:54:45 -0500

    Section 12 - Refund and Complete Order

--- AbbyWeb/Pages/Admin/Order/OrderDetails.cshtml.cs
+++ AbbyWeb/Pages/Admin/Order/OrderDetails.cshtml.cs
@@ -1,8 +1,10 @@
 using Abby.DataAccess.Repository.IRepository;
 using Abby.Models;
 using Abby.Models.ViewModel;
+using Abby.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -24,6 +26,37 @@ namespace AbbyWeb.Pages.Admin.Order
                 OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == id, includeProperties: "ApplicationUser"),
                 OrderDetails = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == id).ToList()
             };
+        }
+
+        public IActionResult OnPostOrderCompleted(int orderId)
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(orderId, SD.StatusCompleted);
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
+        }
+        public IActionResult OnPostOrderRefund(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+
+            var options = new RefundCreateOptions
+            {
+                Reason = RefundReasons.RequestedByCustomer,
+                PaymentIntent = orderHeader.PaymentIntentId
+            };
+
+            var service = new RefundService();
+            Refund refund = service.Create(options);
+
+            _unitOfWork.OrderHeader.UpdateStatus(orderId, SD.StatusRefunded);
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
+        }
+
+        public IActionResult OnPostOrderCancel(int orderId)
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(orderId, SD.StatusCancelled);
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
         }
     }
 }
