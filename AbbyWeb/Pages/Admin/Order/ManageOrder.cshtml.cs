4625b6ea9711ba212f58a4deee916156ee409eb0
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-22 10:53:44 -0500

    Section 12 - Manage Order Get Handler and Status Flow

--- AbbyWeb/Pages/Admin/Order/ManageOrder.cshtml.cs
+++ AbbyWeb/Pages/Admin/Order/ManageOrder.cshtml.cs
@@ -1,12 +1,18 @@
 using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
+using Abby.Models.ViewModel;
+using Abby.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AbbyWeb.Pages.Admin.Order
 {
     public class ManageOrderModel : PageModel
     {
         private readonly IUnitOfWork _unitOfWork;
+        public List<OrderDetailVM> OrderDetailVM { get; set; }
 
         public ManageOrderModel(IUnitOfWork unitOfWork)
         {
@@ -14,6 +20,20 @@ namespace AbbyWeb.Pages.Admin.Order
         }
         public void OnGet()
         {
+            OrderDetailVM = new();
+
+            List<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.Status == SD.StatusSubmitted ||
+            u.Status == SD.StatusInProcess).ToList();
+
+            foreach(OrderHeader item in orderHeaders)
+            {
+                OrderDetailVM individual = new OrderDetailVM()
+                {
+                    OrderHeader = item,
+                    OrderDetails = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == item.Id).ToList()
+                };
+                OrderDetailVM.Add(individual);
+            }
         }
     }
 }
