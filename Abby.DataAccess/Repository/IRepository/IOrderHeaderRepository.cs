d551d8e9bfbf3f14f0f319b7c1690af68987d1cf
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-22 11:09:20 -0500

    Section 12 - Toggle status display button

--- Abby.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ Abby.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -10,6 +10,7 @@ namespace Abby.DataAccess.Repository.IRepository
     public interface IOrderHeaderRepository : IRepository<OrderHeader>
     {
         void Update(OrderHeader obj);
+        void UpdateStatus(int id, string status);
        
     }
 }
--- Abby.DataAccess/Repository/OrderHeaderRepository.cs
+++ Abby.DataAccess/Repository/OrderHeaderRepository.cs
@@ -22,5 +22,14 @@ namespace Abby.DataAccess.Repository
         {
             _db.OrderHeader.Update(obj);
         }
+
+        public void UpdateStatus(int id, string status)
+        {
+            var orderFromDb = _db.OrderHeader.FirstOrDefault(u => u.Id == id);
+            if (orderFromDb != null)
+            {
+                orderFromDb.Status = status;
+            }
+        }
     }
 }
