0dcfaf6ff7cb4f881011de22387f1ef49dba6ad8
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-22 14:55:37 -0500

    Section 13 - View Components

---
+++ AbbyWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,46 @@
+﻿using Abby.DataAccess.Repository.IRepository;
+using Abby.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AbbyWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            int count = 0;
+            if (claim != null)
+            {
+                //user is logged in
+                if (HttpContext.Session.GetInt32(SD.SessionCart) != null)
+                {
+                    return View(HttpContext.Session.GetInt32(SD.SessionCart));
+                }
+                else
+                {
+                    count = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList().Count;
+                    HttpContext.Session.SetInt32(SD.SessionCart, count);
+                    return View(count);
+                }
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                //user has not logged in
+                return View(count);
+            }
+        }
+    }
+}
