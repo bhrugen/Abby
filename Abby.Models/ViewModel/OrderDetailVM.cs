85f229a0525e5c7cb5633a3a23fa525e8714f803
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-22 09:45:17 -0500

    Section 12 - Loading Get Handler

---
+++ Abby.Models/ViewModel/OrderDetailVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abby.Models.ViewModel
+{
+    public class OrderDetailVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public List<OrderDetails> OrderDetails { get; set; }
+
+    }
+}
--- AbbyWeb/Pages/Admin/Order/OrderDetails.cshtml.cs
+++ AbbyWeb/Pages/Admin/Order/OrderDetails.cshtml.cs
@@ -1,12 +1,29 @@
+using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
+using Abby.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AbbyWeb.Pages.Admin.Order
 {
     public class OrderDetailsModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+        
+        public OrderDetailVM OrderDetailVM { get; set; }
+        public OrderDetailsModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public void OnGet(int id)
         {
+            OrderDetailVM = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == id, includeProperties: "ApplicationUser"),
+                OrderDetails = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == id).ToList()
+            };
         }
     }
 }
