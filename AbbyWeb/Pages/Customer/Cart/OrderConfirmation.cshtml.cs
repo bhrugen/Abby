8de7f3cfd5b8af53a58d42f7a5848a305fb5e83b
Bhrugen Patel <bhrugen1990@gmail.com>
2021-09-21 21:02:08 -0500

    Section 11 - Order Confirmation in Action

--- AbbyWeb/Pages/Customer/Cart/OrderConfirmation.cshtml.cs
+++ AbbyWeb/Pages/Customer/Cart/OrderConfirmation.cshtml.cs
@@ -1,12 +1,41 @@
+using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
+using Abby.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe.Checkout;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AbbyWeb.Pages.Customer.Cart
 {
     public class OrderConfirmationModel : PageModel
     {
-        public void OnGet()
+        private readonly IUnitOfWork _unitOfWork;
+        public int OrderId { get; set; }
+        public OrderConfirmationModel(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+        }
+        public void OnGet(int id)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == id);
+            if (orderHeader.SessionId != null)
+            {
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    orderHeader.Status = SD.StatusSubmitted;
+                    _unitOfWork.Save();
+                }
+            }
+            List<ShoppingCart> shoppingCarts = 
+                _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == orderHeader.UserId).ToList();
+            _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+            _unitOfWork.Save();
+            OrderId = id;
+
         }
     }
 }
